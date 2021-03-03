be6231d837e81b5472a687e124f3af230b3c067e
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-03 10:35:32 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,28 @@
+battery_limit = {
+     'temperature': {'min': 0, 'max': 45},
+     'soc': {'min': 20, 'max': 80},
+     'roc': {'min': 0,'max': 0.8}
+        } 
+def battery_range_parameters(battery_Values,parameter_off_Limit):
+  parameter_name=battery_Values.name
+  parameter_value=battery_Values.value
+  
+    if (parameter_value < battery_limit[parameter_name]['min']) or (parameter_value >battery_limit[parameter_name]['max']):
+            parameter_off_Limits.append(parameter_name)
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
+def battery_is_ok(battery_Values):
+  parameter_off_Limit=[]
+  result=battery_range_parameters(battery_Values,parameter_off_Limit)
+  if len(result)==0 :
+    print('battery working condition are good')
+    return true
+  else: 
+    print('Battery maybe having some troubles! Please check')
     return False
 
   return True
 
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok({'temperature': 25,'soc': 70, 'roc': 0.7}) is True)
+  assert(battery_is_ok({'temperature': 50,'soc': 85, 'roc': 0}) is False)
