2bb46bb4879bb64542a1840d0ed661400b98fc15
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-03 10:51:49 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -3,16 +3,12 @@ battery_limit = {
      'soc': {'min': 20, 'max': 80},
      'roc': {'min': 0,'max': 0.8}
         } 
-def battery_range_parameters(battery_Values,parameter_off_Limit):
-  for parameter_name,parameter_value in battery_Values.items() :
-     if (parameter_value < battery_limit[parameter_name]['min']) or (parameter_value >battery_limit[parameter_name]['max']):
-          parameter_off_Limits.append(parameter_name)
-          return parameter_off_Limits
 
 def battery_is_ok(battery_Values):
-  parameter_off_Limit=[]
-  result=battery_range_parameters(battery_Values,parameter_off_Limit)
-  if len(result)==0 :
+ for parameter_name,parameter_value in battery_Values.items() :
+     if (parameter_value < battery_limit[parameter_name]['min']) or (parameter_value >battery_limit[parameter_name]['max']):
+          parameter_off_Limits.append(parameter_name)
+  if len(parameter_off_Limits)==0 :
     print('battery working condition are good')
     return true
   else: 
