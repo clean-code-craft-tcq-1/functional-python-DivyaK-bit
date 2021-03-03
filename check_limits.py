5d02b6a0819e64641da27e04da9f8bf40d034dce
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-03 10:43:03 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -4,8 +4,7 @@ battery_limit = {
      'roc': {'min': 0,'max': 0.8}
         } 
 def battery_range_parameters(battery_Values,parameter_off_Limit):
-  parameter_name=battery_Values.name
-  parameter_value=battery_Values.value
+  for parameter_name,parameter_value in battery_Values.items() :
      if (parameter_value < battery_limit[parameter_name]['min']) or (parameter_value >battery_limit[parameter_name]['max']):
           parameter_off_Limits.append(parameter_name)
 
